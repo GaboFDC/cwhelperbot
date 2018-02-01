09934c160f5505681850f7804754c067de22bd87
Gabriel Diaz <gabriel.diaz@youse.com.co>
2018-02-01 17:28:39 -0500

    Migrated from config file, to env variables

--- botmodules/admin.py
+++ botmodules/admin.py
@@ -7,7 +7,7 @@ from config import *
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    if info['admin_cid']:
+    if os.environ['TG_ADMIN_UID'] == message.from_user.id:
         bot.reply_to(message, message.text)
     else:
         bot.reply_to(message, responses['not_admin'] % m.from_user.first_name)
--- config.py
+++ config.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import os
 import json
 import telebot
 import logging
 
 # Bot config
-info = json.load(open('data/info.json', 'r'))
-responses = json.load(open('data/responses.json', 'r'))
-
-bot = telebot.TeleBot(info['token'])
+bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 import botmodules
 
-if info['log'] == "1":
+if os.environ['LOG_ENABLED'] == "1":
     print("Logeo Activado...")
     logger = telebot.logger
     # Outputs debug messages to console.
@@ -21,12 +19,12 @@ else:
     print("Logeo no activado...")
 
 # Webhook config
-WEBHOOK_HOST = info['host']
-WEBHOOK_PORT = info['port']
+WEBHOOK_HOST = os.environ['WEBHOOK_HOST']
+WEBHOOK_PORT = int(os.environ['WEBHOOK_PORT'])
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = info['ssl_cert']
-WEBHOOK_SSL_PRIV = info['ssl_priv']  # Path to the ssl private key
+WEBHOOK_SSL_CERT = os.environ['WEBHOOK_SSL_CERT']
+WEBHOOK_SSL_PRIV = os.environ['WEBHOOK_SSL_PRIV']  # Path to the ssl private key
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (info['token'])
+WEBHOOK_URL_PATH = "/%s/" % (os.environ['BOT_TOKEN'])
