6eb6f1d933b7414608c3c9990f5646939e30d7d2
Gabriel Diaz <gabriel.diaz@youse.com.co>
2018-02-01 18:18:20 -0500

    Updated message parser

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def index():
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data()
+        json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
-        bot.process_new_messages([update.message])
+        bot.process_new_updates([update])
         return ''
     else:
         abort(403)
