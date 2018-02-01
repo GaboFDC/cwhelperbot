7cc4379aee121ed8902a388a72a23ad41e38964b
Gabriel Diaz <gabriel.diaz@youse.com.co>
2018-02-01 17:58:56 -0500

    Rename APP_PORT to PORT for heroku compatibility

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ else:
 # Webhook config
 WEBHOOK_HOST = os.environ['WEBHOOK_HOST']
 WEBHOOK_PORT = int(os.environ['WEBHOOK_PORT'])
-APP_PORT = int(os.environ['APP_PORT'])
+APP_PORT = int(os.environ['PORT'])
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
